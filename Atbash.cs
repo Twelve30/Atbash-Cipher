84628522412ba8dfbfaf9b41293b0bbe8dc5d4d4
Twelve30 <51714848+Twelve30@users.noreply.github.com>
2025-03-16 15:40:43 +0530

    Add files via upload

---
+++ Atbash.cs
@@ -0,0 +1,47 @@
+﻿using System;
+class CipherComp
+{
+    public void Encrypt()
+    {
+        Console.WriteLine("Enter the string");
+        string ptxt = Console.ReadLine(), str = "";
+        try
+        {
+            for (int i = 0; i < ptxt.Length; i++)
+            {
+                char ch = ptxt[i], mx;
+                if (char.IsLetter(ch) & char.IsLower(ch))
+                {
+                    int lg = 219 - ch;
+                    mx = (char)lg;
+                    str += mx;
+                }
+                else if (char.IsLetter(ch) & char.IsUpper(ch))
+                {
+                    int ug = 155 - ch;
+                    mx = (char)ug;
+                    str += mx;
+                }
+                else
+                {
+                    str += ch;
+                }
+            }
+            Console.WriteLine("The encrypted statement is " + str);
+            Console.WriteLine("========================================================================");
+        }
+        catch { Console.WriteLine("Error"); }
+    }
+}
+class MainProg
+{
+    public static void Main()
+    {
+        Console.WriteLine("Atbash Cipher Command Line");
+        CipherComp ce = new CipherComp();
+        for (; ; )
+        {
+            ce.Encrypt();
+        }
+    }
+}
